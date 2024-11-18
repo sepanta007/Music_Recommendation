013a2da935490f24e2b740bc0e6cea07df55ddf3
Sepanta007 <sepantaff7900@gmail.com>
2024-11-18 11:55:07 +0100

    Add artist_id to the dataset

--- src/preperation.py
+++ src/preperation.py
@@ -7,11 +7,6 @@ music_data = pd.read_csv(file_path)
 # Assign unique IDs to each unique (track_name, artist_name) combination
 music_data['track_id'] = music_data[['track_name', 'artist_name']].apply(tuple, axis=1).factorize()[0] + 1
 
-# Save the modified DataFrame back to the CSV file
-music_data.to_csv(file_path, index=False)  # Overwrites the original file
-
-print("The track_id column has been added to the CSV file, considering both track_name and artist_name.")
-
 # Define genre mapping
 genre_mapping = {
     'pop': 1,
@@ -26,7 +21,10 @@ genre_mapping = {
 # Map genres to their corresponding IDs
 music_data['genre_id'] = music_data['genre'].map(genre_mapping)
 
-# Save the modified DataFrame back to the CSV file
+# Assign unique IDs to each unique artist_name
+music_data['artist_id'] = music_data['artist_name'].factorize()[0] + 1
+
+# Save the modified DataFrame back to the CSV file (single save to optimize)
 music_data.to_csv(file_path, index=False)  # Overwrites the original file
 
-print("The genre_id column has been added to the CSV file based on the specified genre mapping.")
+print("The track_id, genre_id, and artist_id columns have been added to the CSV file.")
